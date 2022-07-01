a71de1a972f5bedf4f016eba26b35ccc107fdd7b
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-07-01 12:32:49 +0100

    added toggle to desktop menu

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ let showMenu = document.querySelector("#showMenu");
 let closeMenu = document.querySelector("#closeMenu");
 let showMobileMenu = false;
 
+let features = document.querySelector("#features");
+let featuresMenu = document.querySelector("#featuresMenu");
+let featuresArrow = document.querySelector("#featureArrow");
+let showFeaturesMenu = false;
+
+let company = document.querySelector("#company");
+let companyMenu = document.querySelector("#companyMenu");
+let companyArrow = document.querySelector("#companyArrow");
+let showCompanyMenu = false;
+
+company.addEventListener("click", function () {
+  showCompanyMenu = !showCompanyMenu;
+  toggleMenu(companyMenu, companyArrow, showCompanyMenu);
+});
+
+features.addEventListener("click", function () {
+  showFeaturesMenu = !showFeaturesMenu;
+  toggleMenu(featuresMenu, featuresArrow, showFeaturesMenu);
+});
+
 showMenu.addEventListener("click", function () {
   showMobileMenu = !showMobileMenu;
   toggle(mobileMenu, showMobileMenu);
@@ -20,5 +40,15 @@ function toggle(selector, boolX) {
   } else {
     selector.classList.add("hidden");
     return;
+  }
+}
+
+function toggleMenu(menu, arrowID, showBool) {
+  if (showBool) {
+    menu.classList.remove("hidden");
+    arrowID.src = "./images/icon-arrow-down.svg";
+  } else {
+    menu.classList.add("hidden");
+    arrowID.src = "./images/icon-arrow-up.svg";
   }
 }
